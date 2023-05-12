611ea640065ca769329903a6f669efe3222c10e6
juank <juanrealpe972@gmail.com>
2023-05-12 13:54:30 -0500

    new

--- js/ficha.js
+++ js/ficha.js
@@ -1,3 +1,47 @@
+function almacenarData(){
+    let nom,lin,ant,act,mod,gen,des,har,sof,otr,fun,adi,cli
+    nom = document.getElementById("nombre").value
+    lin = document.getElementById("linea").value
+    ant = document.getElementById("anterior").value
+    act = document.getElementById("actual").value
+    mod = document.getElementById("modulo").value
+    gen = document.getElementById("general").value
+    des = document.getElementById("descripcion").value
+    har = document.getElementById("hardware").value
+    sof = document.getElementById("software").value
+    otr = document.getElementById("otro").value
+    fun = document.getElementById("funcional").value
+    adi = document.getElementById("adicional").value
+    cli = document.getElementById("cliente").value
+
+    localStorage.setItem("Nombre", nom);
+    localStorage.setItem("Linea", lin);
+    localStorage.setItem("Anterior", ant);
+    localStorage.setItem("Actual", act);
+    localStorage.setItem("Modulo", mod);
+    localStorage.setItem("General", gen);
+    localStorage.setItem("Descripcion", des);
+    localStorage.setItem("Hardware", har);
+    localStorage.setItem("Software", sof);
+    localStorage.setItem("Otro", otr);
+    localStorage.setItem("Funcional", fun);
+    localStorage.setItem("Adicional", adi);
+    localStorage.setItem("Cliente", cli);
+
+    document.getElementById("nombre").value ="";
+    document.getElementById("linea").value ="";
+    document.getElementById("anterior").value ="";
+    document.getElementById("actual").value ="";
+    document.getElementById("modulo").value ="";
+    document.getElementById("general").value ="";
+    document.getElementById("descripcion").value ="";
+    document.getElementById("hardware").value ="";
+    document.getElementById("software").value ="";
+    document.getElementById("otro").value ="";
+    document.getElementById("funcional").value ="";
+    document.getElementById("adicional").value ="";
+    document.getElementById("cliente").value ="";
+}
 const a = document.querySelector("#datoProducto")
 const b = document.querySelector("#datoLinea")
 const c = document.querySelector("#datoAnterior")
@@ -5,7 +49,7 @@ const d = document.querySelector("#datoActual")
 const e = document.querySelector("#datoModulo")
 const f = document.querySelector("#datoGeneral")
 const g = document.querySelector("#datoDescripcion")
-const h = document.querySelector("#datoHadware")
+const h = document.querySelector("#datoHardware")
 const i = document.querySelector("#datoSoftware")
 const j = document.querySelector("#datoOtro")
 const k = document.querySelector("#datoFuncional")
@@ -13,21 +57,21 @@ const l = document.querySelector("#datoAdicional")
 const m = document.querySelector("#datoCliente")
 
 function cargarData(){
-    let nom,lin,ant,act,mod,gen,des,had,sof,otr,fun,adi,cli
+    let nom,lin,ant,act,mod,gen,des,har,sof,otr,fun,adi,cli;
 
-    nom = localStorage.getItem("nombre");
-    lin = localStorage.getItem("linea");
-    ant = localStorage.getItem("anterior");
-    act = localStorage.getItem("actual");
-    mod = localStorage.getItem("modulo");
-    gen = localStorage.getItem("general");
-    des = localStorage.getItem("descripcion");
-    had = localStorage.getItem("hadware");
-    sof = localStorage.getItem("software");
-    otr = localStorage.getItem("otro");
-    fun = localStorage.getItem("funcional");
-    adi = localStorage.getItem("adicional");
-    cli = localStorage.getItem("cliente");
+    nom = localStorage.getItem("Nombre");
+    lin = localStorage.getItem("Linea");
+    ant = localStorage.getItem("Anterior");
+    act = localStorage.getItem("Actual");
+    mod = localStorage.getItem("Modulo");
+    gen = localStorage.getItem("General");
+    des = localStorage.getItem("Descripcion");
+    har = localStorage.getItem("Hardware");
+    sof = localStorage.getItem("Software");
+    otr = localStorage.getItem("Otro");
+    fun = localStorage.getItem("Funcional");
+    adi = localStorage.getItem("Adicional");
+    cli = localStorage.getItem("Cliente");
 
     a.innerHTML = nom;
     b.innerHTML = lin;
@@ -36,14 +80,10 @@ function cargarData(){
     e.innerHTML = mod;
     f.innerHTML = gen;
     g.innerHTML = des;
-    h.innerHTML = had;
+    h.innerHTML = har;
     i.innerHTML = sof;
     j.innerHTML = otr;
     k.innerHTML = fun;
     l.innerHTML = adi;
     m.innerHTML = cli;
-}
-
-function cargarData (){
-    
 }
